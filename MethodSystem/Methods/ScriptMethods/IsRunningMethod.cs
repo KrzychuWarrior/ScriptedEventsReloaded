99c20d222ac9e2eb0fafbcae1cb27057a85a4335
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2025-11-08 09:27:49 +0100

    Update IsRunningMethod.cs

--- MethodSystem/Methods/ScriptMethods/IsRunningMethod.cs
+++ MethodSystem/Methods/ScriptMethods/IsRunningMethod.cs
@@ -3,24 +3,22 @@ using SER.ArgumentSystem.BaseArguments;
 using SER.MethodSystem.BaseMethods;
 using SER.ValueSystem;
 using SER.ScriptSystem;
-using System.Collections.Generic;
 using System.Linq;
 
-namespace SER.MethodSystem.Methods.ScriptMethods
+namespace SER.MethodSystem.Methods.ScriptMethods;
+
+public class IsRunningMethod : ReturningMethod<BoolValue>
 {
-    public class IsRunningMethod : ReturningMethod<BoolValue>
+    public override string Description => "Returns true if given script is running";
+
+    public override Argument[] ExpectedArguments => 
+    [
+        new TextArgument("script name")
+    ];
+
+    public override void Execute()
     {
-        public override string Description => "Returns true if given script is running";
-
-        public override Argument[] ExpectedArguments => 
-        [
-            new TextArgument("script name")
-        ];
-
-        public override void Execute()
-        {
-            var name = Args.GetText("script name");
-            ReturnValue = Script.RunningScripts.Any(scr => scr.Name == name);
-        }
+        var name = Args.GetText("script name");
+        ReturnValue = Script.RunningScripts.Any(scr => scr.Name == name);
     }
 }
